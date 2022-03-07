69d44a0c3ac31f9979b8601de8363d447850712a
AlvaroPerezRey <aperez.alumno@iesrafaelalberti.es>
2022-03-07 20:33:00 +0100

    Primeras rutas

--- aplicacion/app.py
+++ aplicacion/app.py
@@ -1,7 +1,21 @@
-import re
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+app = Flask(__name__)	
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def inicio():
+    return 'Página principal'
+
+@app.route('/articulos/')
+def articulos():
+    return 'Lista de artículos'
+
+@app.route('/articulos/new',methods=["POST"])
+def articulos_new():
+	return 'Está URL recibe información de un formulario con el método POST'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'Hemos accedido con POST'
+    else:
+        return 'Hemos accedido con GET'
